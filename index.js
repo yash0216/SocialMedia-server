6d7d6455b1515d2951a69b02c68061959cc83fc2
Yash Pandey <pandeyyash613@gmail.com>
2024-05-25 13:04:14 +0530

    save the cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(morgan("common"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://social-media-client-seven.vercel.app",
     credentials: true,
   })
 );
